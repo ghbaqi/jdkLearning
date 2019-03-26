5476ef0b7d52ba837af813aff03a0f8b3c9dd7bb
gaohui <1562073264@qq.com>
2019-03-26 16:27:56 +0800

    mybatis 源码

---
+++ src/main/java/com/learn/mybatis/demo/Demo1.java
@@ -0,0 +1,51 @@
+package com.learn.mybatis.demo;
+
+import com.learn.mybatis.mapper.LearnResourceMapper;
+import com.learn.mybatis.pojo.LearnResource;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.InputStream;
+
+/**
+ * @author : gaohui
+ * @Date : 2019/3/26 09:18
+ * @Description :
+ * <p>
+ * 入门程序
+ */
+
+/**
+ *
+ */
+
+/**
+ * 踩坑
+ * 路径不能使用 classpath:mybatis-config.xml
+ */
+public class Demo1 {
+
+
+    private static final String resource = "mybatis-config.xml";
+
+    public static void main(String[] args) throws Exception {
+
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+
+        SqlSession session = sessionFactory.openSession();
+        
+
+        LearnResourceMapper mapper = session.getMapper(LearnResourceMapper.class);
+        LearnResource learnResource = mapper.selectByPrimaryKey(999L);
+        System.out.println(learnResource);
+
+        session.close();
+    }
+
+
+}
--- src/main/java/com/learn/thread/Application.java
+++
@@ -1,8 +0,0 @@
-package com.learn.thread;
-/**
- * @author : gaohui
- * @Date : 2019/2/26 15:36
- * @Description :
- */
-public class Application {
-}
