4152612497353a526394725f71b028e224a7372a
gaohui <1562073264@qq.com>
2019-03-29 18:10:45 +0800

    hashmap 解析

---
+++ src/main/java/com/learn/hashmap/Demo01.java
@@ -0,0 +1,22 @@
+package com.learn.hashmap;
+
+import java.util.HashMap;
+
+/**
+ * @author : gaohui
+ * @Date : 2019/3/29 11:57
+ * @Description :
+ */
+public class Demo01 {
+
+    public static void main(String[] args) {
+
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        hashMap.put("aa", 1);
+        hashMap.put("bb", 2);
+        hashMap.put("cc", 3);
+        System.out.println(hashMap.get("cc"));
+
+    }
+
+}
